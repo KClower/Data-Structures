ed820611ae7cc91808affed1b18853f8030cc05d
KClower <kclower3@gmail.com>
2024-10-23 10:58:06 -0500

    singly_linked_list is complete along with a breakout challenge(that is not tested)

--- stack/stack.py
+++ stack/stack.py
@@ -9,6 +9,7 @@ return elements in Last In First Out order.
    Make sure the Stack tests pass.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
+                    Either would work.
 """
 import sys
 sys.path.append('./doubly_linked_list')
